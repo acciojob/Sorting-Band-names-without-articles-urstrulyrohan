032371383ac116cfce1d01495c88cfdd9bfabfcb
Rohan Pande <131426445+urstrulyrohan@users.noreply.github.com>
2023-09-08 23:08:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
-
+let regexp = /\bthe\b|\ban\b|\ba\b/gi;
+console.log(str.replace(regexp, "$"));
+let arr = [
+	'The Virupaksha Temple', 
+	'a Victoria Memorial',
+	'an Tajmahal'
+];
+let articleLessArray = [];
+for (let i = 0; i < arr.lenghth ; i++) {
+	let articleLessItem = arr[i].replace(regexp , "").trim();
+	articleLessArray.push(articleLessItem)
+}
+articleLessArray.sort();
+console.log(articleLessArray);
